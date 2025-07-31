7986a845cdf722d465f1d42c972e31682be83e48
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-07-31 16:26:29 +0900

    feat: OrderItem 엔티티 구현 (생성/취소/금액 계산 비즈니스 로직 포함)
    - createOrderItem() 정적 생성 메서드
    - cancel()을 통한 재고 복구
    - getTotalPrice()로 금액 계산 로직 구현

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -24,4 +24,26 @@ public class OrderItem {
     private int orderPrice;
 
     private int count;
+
+    // 생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        // 주문한 수량만큼 상품의 재고 감소
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // 비즈니스 로직
+    public void cancel() {
+        // 취소한 주문 수량만큼 상품의 재고 증가
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
