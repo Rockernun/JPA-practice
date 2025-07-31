2307b06b12b84574e359436e0103170191785991
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-07-31 17:11:12 +0900

    feat: OrderService 클래스 구현
    - 회원 ID와 상품 ID, 수량을 받아 주문을 생성하는 order 메서드 구현
    - 주문 ID를 기반으로 주문을 취소하는 cancel 메서드 구현
    - 엔티티 조회 및 생성 메서드 활용으로 비즈니스 로직 구성

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,62 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    // 주문
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        // 엔티티 조회
+        Member member = memberRepository.findMember(memberId);
+        Item item = itemRepository.findById(itemId);
+
+        // 배송 정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문 상품 생성 (생성 메서드 사용)
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    // 취소
+    @Transactional
+    public void cancel(Long orderId) {
+        // 주문 엔티티 조회 후 주문 취소
+        Order order = orderRepository.findOrder(orderId);
+        order.cancel();
+    }
+
+    // 검색
+//    public List<Order> searchOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
+}
